6353294ddfa7bf385d7641742d083c5e861bd0c7
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2020-11-14 12:43:17 +0600

    SecondActivity App

---
+++ Assignments/Lab/TwoScreenApp/app/src/main/java/com/mahfujshaan/cse486/twoscreenapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.mahfujshaan.cse486.twoscreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondActivity();
+            }
+        });
+    }
+    public void openSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+    public void showToast(View view) {
+        Toast.makeText(this, "Login Successful!!!", Toast.LENGTH_SHORT).show();
+
+    }
+}
