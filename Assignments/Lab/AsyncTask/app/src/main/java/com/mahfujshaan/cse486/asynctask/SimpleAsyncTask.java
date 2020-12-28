b8197716355c4b329e49608d5a7a06b8d8c824b3
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2020-12-28 14:51:03 +0600

    AsyncTask
    Lab 09 completed

---
+++ Assignments/Lab/AsyncTask/app/src/main/java/com/mahfujshaan/cse486/asynctask/SimpleAsyncTask.java
@@ -0,0 +1,42 @@
+package com.mahfujshaan.cse486.asynctask;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
+
+    private WeakReference<TextView> mTextView;
+    private WeakReference<TextView> mResultTextView;
+
+    SimpleAsyncTask (TextView tv, TextView result){
+        mTextView = new WeakReference<>(tv);
+        mResultTextView = new WeakReference<>(result);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        Random random = new Random();
+        int number = random.nextInt(11);
+        int milli = number * 200;
+        publishProgress(milli);
+        try {
+            Thread.sleep(milli);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return "Wake Up after " + milli + " milliseconds";
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        mResultTextView.get().setText("Sleep Time " + values[0] + " ms");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+}
