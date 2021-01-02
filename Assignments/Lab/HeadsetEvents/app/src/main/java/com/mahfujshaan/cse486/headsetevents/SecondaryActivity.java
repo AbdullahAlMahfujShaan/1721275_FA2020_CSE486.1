0bdbde976145022293d6d694d7ed3f2866ac8370
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-02 13:33:59 +0600

    enhancement #4 SecondaryActivity
    Completed SecondaryActivity /  BroadcastReceiver part.

--- Assignments/Lab/HeadsetEvents/app/src/main/java/com/mahfujshaan/cse486/headsetevents/SecondaryActivity.java
+++ Assignments/Lab/HeadsetEvents/app/src/main/java/com/mahfujshaan/cse486/headsetevents/SecondaryActivity.java
@@ -1,6 +1,17 @@
 package com.mahfujshaan.cse486.headsetevents;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class SecondaryActivity extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        String intentAction = intent.getAction();
+        if(intentAction != null){
+            Toast.makeText(context, "Headphones have been unplugged or plugged", Toast.LENGTH_LONG).show();
+        }
+    }
 }
