4fe846600adf74ccde2d71181865143e7d4a93a4
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-04 22:16:56 +0600

    project-progress #6 MainActivity
    ArrayList added. Changed the build gradle, so that binding is easier with the ID.

--- AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/MainActivity.java
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/MainActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mahfujshaan.cse486.thefoodtongapp.Models.MainModel;
+import com.mahfujshaan.cse486.thefoodtongapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+
+ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ArrayList<MainModel> list = new ArrayList<>();
+        list.add(new MainModel(R.drawable.burger, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+
+       //Add Photos and Fix
+        list.add(new MainModel(R.drawable.pizza, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+        list.add(new MainModel(R.drawable.hotdog, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+        list.add(new MainModel(R.drawable.burger, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+        list.add(new MainModel(R.drawable.burger, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+        list.add(new MainModel(R.drawable.burger, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+        list.add(new MainModel(R.drawable.burger, "Burger", "250", "Cheese Burger garnished with Tomato and Honey Mustard"));
+
+
     }
 }
