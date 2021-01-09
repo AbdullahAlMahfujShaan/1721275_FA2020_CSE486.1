bf53ca66e7a550d677757b6bd89938fb38274412
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-09 14:23:04 +0600

    project-progress #8 ViewHolders
    Added ViewHolder models for order function

---
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/Adapters/OrdersAdapter.java
@@ -0,0 +1,30 @@
+package com.mahfujshaan.cse486.thefoodtongapp.Adapters;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mahfujshaan.cse486.thefoodtongapp.R;
+
+public class OrdersAdapter {
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        ImageView orderImage;
+        TextView soldItemName, orderNumber, price;
+
+
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            orderImage = itemView.findViewById(R.id.orderImage);
+            soldItemName = itemView.findViewById(R.id.orderItemName);
+            orderNumber = itemView.findViewById(R.id.orderNumber);
+            price = itemView.findViewById(R.id.orderPrice);
+        }
+    }
+}
