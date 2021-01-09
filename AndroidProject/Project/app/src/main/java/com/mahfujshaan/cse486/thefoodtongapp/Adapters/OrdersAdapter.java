50a3e6417be2299e5df66727d1755bc3167d2b41
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-09 14:25:33 +0600

    project-progress #8 OrdersAdapter
    Added constructors for viewHolder

--- AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/Adapters/OrdersAdapter.java
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/Adapters/OrdersAdapter.java
@@ -1,15 +1,44 @@
 package com.mahfujshaan.cse486.thefoodtongapp.Adapters;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mahfujshaan.cse486.thefoodtongapp.Models.OrdersModel;
 import com.mahfujshaan.cse486.thefoodtongapp.R;
 
-public class OrdersAdapter {
+import java.util.ArrayList;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.viewHolder>{
+
+    ArrayList<OrdersModel> list;
+    Context context;
+
+    public OrdersAdapter(ArrayList<OrdersModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class viewHolder extends RecyclerView.ViewHolder {
 
