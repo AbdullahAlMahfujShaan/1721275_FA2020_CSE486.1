192c1fa91761d0a69e05e06d3ba507346a833dcb
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-13 02:11:55 +0600

    project-progress #11 MainAdapter
    deploying food order progress

--- AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/Adapters/MainAdapter.java
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/Adapters/MainAdapter.java
@@ -1,6 +1,7 @@
 package com.mahfujshaan.cse486.thefoodtongapp.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mahfujshaan.cse486.thefoodtongapp.DetailActivity;
 import com.mahfujshaan.cse486.thefoodtongapp.Models.MainModel;
 import com.mahfujshaan.cse486.thefoodtongapp.R;
 
@@ -38,6 +40,17 @@ final MainModel model = list.get(position);
         holder.mainName.setText(model.getName());
         holder.price.setText(model.getPrice());
         holder.description.setText(model.getDescription());
+holder.itemView.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra("image", model.getImage());
+        intent.putExtra("price", model.getPrice());
+        intent.putExtra("description", model.getDescription());
+        intent.putExtra("name", model.getName());
+        context.startActivity(intent);
+    }
+});
 
     }
 
