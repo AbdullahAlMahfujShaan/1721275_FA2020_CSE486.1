4de84555e72c8d936a69787b9cb1980af5978d49
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-13 23:29:17 +0600

    project-progress #10 Database Initialization

--- AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/DBHelper.java
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/DBHelper.java
@@ -1,5 +1,6 @@
 package com.mahfujshaan.cse486.thefoodtongapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -17,16 +18,42 @@ final static int DBVERSION = 1;
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-   sqLiteDatabase.execSQL();
+   sqLiteDatabase.execSQL(
+           "create table orders" +
+           "(id int primary key autoincrement," +
+           "name text," +
+           "phone text,"+
+           "price int," +
+           "image int," +
+           "foodname text," +
+           "description text)"
+   );
 
 
-
-
-);
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+sqLiteDatabase.execSQL("DROP TABLE IF EXISTS orders");
+onCreate(sqLiteDatabase);
     }
+public boolean insertOrder(String name, String phone, int price, int image, String description, String foodname )
+{
+
+SQLiteDatabase database = getReadableDatabase();
+    ContentValues values = new ContentValues();
+    values.put("name",name);
+    values.put("phone",phone);
+    values.put("price",price);
+    values.put("image",image);
+    values.put("foodname",foodname);
+    values.put("description",description);
+    long id = database.insert("orders",null,values);
+    if(id <=0){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 }
