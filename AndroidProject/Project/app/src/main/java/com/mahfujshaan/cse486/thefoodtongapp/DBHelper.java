8340df3e26354756f48617ae758e5bf9a638f26e
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-21 14:42:39 +0600

    bug-fixes

--- AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/DBHelper.java
+++ AndroidProject/Project/app/src/main/java/com/mahfujshaan/cse486/thefoodtongapp/DBHelper.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
     final static String DBNAME = "foodtongbase.db";
-final static int DBVERSION = 2;
+    final static int DBVERSION = 3;
 
     public DBHelper(@Nullable Context context) {
         super(context, DBNAME, null, DBVERSION);
@@ -21,78 +21,78 @@ final static int DBVERSION = 2;
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-   sqLiteDatabase.execSQL(
-           "create table orders" +
-           "(id integer primary key autoincrement," +
-           "name text," +
-           "phone text,"+
-           "price int," +
-           "image int," +
-                   "quantity int," +
-           "foodname text," +
-           "description text)"
-   );
+        sqLiteDatabase.execSQL(
+                "create table orders" +
+                        "(id integer primary key autoincrement," +
+                        "name text," +
+                        "phone text,"+
+                        "price int," +
+                        "image int," +
+                        "quantity int," +
+                        "foodname text," +
+                        "description text)"
+        );
 
 
     }
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-sqLiteDatabase.execSQL("DROP TABLE IF EXISTS orders");
-onCreate(sqLiteDatabase);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS orders");
+        onCreate(sqLiteDatabase);
     }
-public boolean insertOrder(String name, String phone, int price, int image, String description, String foodname, int quantity)
-{
+    public boolean insertOrder(String name, String phone, int price, int image, String description, String foodname, int quantity)
+    {
 
-SQLiteDatabase database = getReadableDatabase();
-    ContentValues values = new ContentValues();
-    values.put("name",name);
-    values.put("phone",phone);
-    values.put("price",price);
-    values.put("image",image);
-    values.put("description",description);
-    values.put("foodname",foodname);
-    values.put("quantity",quantity);
+        SQLiteDatabase database = getReadableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name",name);
+        values.put("phone",phone);
+        values.put("price",price);
+        values.put("image",image);
+        values.put("description",description);
+        values.put("foodname",foodname);
+        values.put("quantity",quantity);
 
 
-    long id = database.insert("orders",null,values);
-    if(id <=0){
-        return false;
+        long id = database.insert("orders",null,values);
+        if(id <=0){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
-    else {
-        return true;
-    }
-}
 
 
-public ArrayList<OrdersModel> getOrders(){
+    public ArrayList<OrdersModel> getOrders(){
         ArrayList<OrdersModel> orders = new ArrayList<>();
         SQLiteDatabase database = this.getWritableDatabase();
         Cursor cursor = database.rawQuery("Select id,foodname,image,price from orders", null);
-if(cursor.moveToFirst()){
-    while(cursor.moveToNext()){
-        OrdersModel model = new OrdersModel();
-        model.setOrderNumber(cursor.getInt(0)+"");
-        model.setSoldItemName(cursor.getString(1));
-        model.setOrderImage(cursor.getInt(2));
-        model.setPrice(cursor.getInt(3)+"");
-        orders.add(model);
+        if(cursor.moveToFirst()){
+            while(cursor.moveToNext()){
+                OrdersModel model = new OrdersModel();
+                model.setOrderNumber(cursor.getInt(0)+"");
+                model.setSoldItemName(cursor.getString(1));
+                model.setOrderImage(cursor.getInt(2));
+                model.setPrice(cursor.getInt(3)+"");
+                orders.add(model);
+            }
+        }
+        cursor.close();
+        database.close();
+        return orders;
     }
-     }
-cursor.close();
-database.close();
-return orders;
-     }
 
 
-     public Cursor getOrderById(int id){
+    public Cursor getOrderById(int id){
 
-         SQLiteDatabase database = this.getWritableDatabase();
-         Cursor cursor = database.rawQuery("Select * from orders where id ="+id, null);
-if(cursor !=null)
-    cursor.moveToFirst();
-         return cursor;
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery("Select * from orders where id ="+id, null);
+        if(cursor !=null)
+            cursor.moveToFirst();
+        return cursor;
 
-     }
+    }
 
     public boolean updateOrder(String name, String phone, int price, int image, String description, String foodname, int quantity, int id)
     {
